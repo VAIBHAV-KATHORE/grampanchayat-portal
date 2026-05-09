27031d9b198c94e3bef386650f961eebc9d4b923
VAIBHAV-KATHORE <rajdippvtltd@gmail.com>
2026-05-10 03:21:32 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$conn = mysqli_connect(
-    getenv("DB_HOST"),
-    getenv("DB_USER"),
-    getenv("DB_PASS"),
-    getenv("DB_NAME"),
-    getenv("DB_PORT")
-);
+
+$host = "ballast.proxy.rlwy.net";
+$user = "root";
+$pass = "kDaKtDGPpXKsGSaeroDhjLJpSsMUjeDK";
+$db   = "railway";
+$port = 11892;
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
 ?>
