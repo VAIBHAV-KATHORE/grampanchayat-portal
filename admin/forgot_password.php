0df1483a0d0bf7ef2c156be6d588d0928142889a
VAIBHAV-KATHORE <rajdippvtltd@gmail.com>
2026-05-10 05:08:56 +0530

    Update forgot_password.php

--- admin/forgot_password.php
+++ admin/forgot_password.php
@@ -38,45 +38,47 @@ if (isset($_POST['send'])) {
 
         $apiKey = "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";
 
-        $emailData = [
-            "personalizations" => [[
-                "to" => [[ "email" => $email ]]
-            ]],
-            "from" => [
-                "email" => "kathorevaibhav5791@gmail.com",
-                "name" => "Admin System"
-            ],
-            "subject" => "Reset Your Password",
-            "content" => [[
-                "type" => "text/html",
-                "value" => "
-                    <h2>Password Reset Request</h2>
-                    <p>Click below link to reset your password:</p>
-                    <a href='$reset_link'>Reset Password</a>
-                    <p>This link is valid for one-time use only.</p>
-                "
-            ]]
-        ];
+$apiKey = "YOUR_NEW_SENDGRID_API_KEY"; // 🔴 REGENERATE THIS
 
-        $ch = curl_init();
+$emailData = [
+    "personalizations" => [[
+        "to" => [[ "email" => $email ]]
+    ]],
+    "from" => [
+        "email" => "kathorevaibhav5791@gmail.com",
+        "name" => "Grampanchayat Portal"
+    ],
+    "subject" => "Reset Your Password",
+    "content" => [[
+        "type" => "text/html",
+        "value" => "
+            <h2>Password Reset Request</h2>
+            <p>Click below link:</p>
+            <a href='$reset_link'>Reset Password</a>
+        "
+    ]]
+];
 
-        curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($emailData));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Authorization: Bearer $apiKey",
-            "Content-Type: application/json"
-        ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($emailData));
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Authorization: Bearer $apiKey",
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $response = curl_exec($ch);
-        curl_close($ch);
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
 
-        $msg = "<div style='color:green;'>Reset link sent successfully</div>";
-
-    } else {
-        $msg = "<div style='color:red;'>Email not found</div>";
-    }
+// 🔴 DEBUG IMPORTANT
+if ($http_code == 202) {
+    $msg = "<div style='color:green;'>Email sent successfully</div>";
+} else {
+    $msg = "<div style='color:red;'>Failed to send email. Response: $response</div>";
+}
 }
 ?>
 
